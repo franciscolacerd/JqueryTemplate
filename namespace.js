41e35202b640291081cdaf70e6c3eb0af9e053ef
Francisco Lacerda <francisco.pedro.lacerd@gmail.com>
2022-05-10 14:15:34 +0100

    Create namespace.js

---
+++ namespace.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $.fn.Template = function () {
+        var _this = this;
+
+        _this.obj = {};
+
+        _this.createElements = function (o) {
+            o.els.TemplateElement = o.el.find('.template-element');
+        };
+
+        _this.createFunctions = function (o) {
+            o.templateFunction = (o, val) => o ? val : null;
+        };
+
+        _this.createEvents = function (o) {
+            o.els.TemplateElement.on('click', function () { console.log('click'); })
+        };
+
+        _this.render = function (o) { };
+
+        _this.output = {
+            templateFunction: function (val) { _this.obj.templateFunction(_this.obj, val); },
+        };
+
+        _this.init = function () {
+            var obj = {
+                el: $(this),
+                els: {},
+                vars: {}
+            };
+
+            _this.obj = obj;
+
+            _this.createElements(obj);
+
+            _this.createFunctions(obj);
+
+            _this.createEvents(obj);
+
+            _this.render(obj);
+        };
+
+        this.each(_this.init);
+
+        return _this.output;
+    };
+});
